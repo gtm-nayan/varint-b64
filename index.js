d11787483a0fc4f4246d06c3b86cdf5d40872be9
Nayan Gautam <gtmnayan@gmail.com>
2023-10-29 11:40:07 +0545

    fix

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ export const encode = (num) =>
     (num & 31) | // take the last 5 bits
       // @ts-expect-error
       ((num > 31) << 5) // if num is greater than 31, set the 6th bit
-  ] + (num >>> 5 ? encode(num) : ""); // if num was greater than 31, encode the remaining bits
+  ] + ((num >>>= 5) ? encode(num) : ""); // if num was greater than 31, encode the remaining bits

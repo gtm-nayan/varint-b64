e2c14337d8471e8eae107bf10b455241ef523694
Nayan Gautam <gtmnayan@gmail.com>
2023-10-29 14:41:42 +0545

    tweak encoding to favour stable prefix for lower numbers

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const dictionary =
   "etaoinsrhdlucmfywgpbvkxqjz012345ETAOINSRHDLUCMFYWGPBVKXQJZ6789._";
 /**
  * @param {number} num
+ * @param {number} [recursed] Internal use only, do not pass in.
  * @returns {string}
  */
-export const encode = (num) =>
-  dictionary[
-    (num & 31) | // take the last 5 bits
-      // @ts-expect-error
-      ((num > 31) << 5) // if num is greater than 31, set the 6th bit
-  ] + ((num >>>= 5) ? encode(num) : ""); // if num was greater than 31, encode the remaining bits
+export const encode = (num, recursed) =>
+  (num > 31 ? encode((num >>> 5) - 1, 1) : "") +
+  // @ts-expect-error
+  dictionary[(recursed << 5) | (num & 31)];

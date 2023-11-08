83079d577a75b799dcd57c65c05769d053a3a347
Nayan Gautam <gtmnayan@gmail.com>
2023-11-08 20:34:26 +0545

    tweak dictionary again
    dot is not a valid css selector

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 /**
  * @param {number} num
- * @param {number} [recursed] Internal use only, do not pass in.
  * @returns {string}
  */
-export const encode = (num, recursed) =>
+export const encode = (
+  num,
+  // @ts-expect-error
+  recursed
+) =>
   (num > 31 ? encode((num >>> 5) - 1, 1) : "") +
-  "etaoinsrhdlucmfywgpbvkxqjz012345ETAOINSRHDLUCMFYWGPBVKXQJZ6789._"[
-    // @ts-expect-error
+  "etaoinsrhdlucmfywgpbvkxqjz0123-_ETAOINSRHDLUCMFYWGPBVKXQJZ456789"[
     (recursed << 5) | (num & 31)
   ];
